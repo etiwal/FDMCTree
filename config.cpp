a285b39fa83438f286041dc05790fd6510ccc184
etiwal <etienne.walther@outlook.com>
2020-11-03 21:15:34 +0100

    new version

--- config.cpp
+++ config.cpp
@@ -8,19 +8,19 @@ namespace config
 {
 	int sim_time = 100;
 
-	int rollouts = 20;
-	int horizon = 10;
+	int rollouts = 100;
+	int horizon = 20;
 
 	std::vector<int> expert_types = {0, 1, 2};
 	std::vector<int> expert_weights = {1, 1, 1};
 
 	int obstacle_cost = 10000;
-	std::vector<int> obstacle_rad = {5, 8, 5, 5};
-	std::vector<int> obstacle_pos = {20, 10, 40, 30, 10, 25, 100, 40};
+	std::vector<int> obstacle_rad = {10, 5, 10, 5};
+	std::vector<int> obstacle_pos = {5, 20, 55, 15, 40, 10, 100, 40};
 
 	extern std::vector<double> target_state = {100, 50, 0, 0, 0, 0};
 
-	double pruning_threshold = 1.5;
+	double pruning_threshold = 1.4;
 
 	bool log_sampling = true;
 }
