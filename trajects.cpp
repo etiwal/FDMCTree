05bb9912632502321d7b1e6f7d99c58d375c5161
etiwal <etienne.walther@outlook.com>
2020-11-15 14:42:58 +0100

    added trajectory class

--- trajects.cpp
+++ trajects.cpp
@@ -3,4 +3,13 @@
 //
 #include "trajects.h"
 
+// Traject
+Traject::Traject(size_t sim_step, size_t rank): node_vec_(config::horizon){
+	traject_length_ = config::horizon;
+	sim_step_ = sim_step;
+	rank_ = rank;
+}
 
+void Traject::append(const Node& node_object){
+	node_vec_.push_back(node_object);
+}
--- trajects.h
+++ trajects.h
@@ -10,23 +10,31 @@
 #include <vector>
 #include "fdmcts.h"
 
+struct Traject{
+public:
+	Traject(size_t sim_step, size_t rank);
+	~Traject() = default;
+
+	void append(const Node& node_object);
+
+	size_t horizon_step_;
+	size_t sim_step_;
+	size_t rank_;
+
+
+private:
+	size_t traject_length_;
+	std::vector<Node> node_vec_;
+};
+
 struct Trajects{
 public:
 	Trajects();
 	~Trajects() = default;
 
-	std::vector<Node> get_best_traj_prev();
+	Traject get_best_prev_traject();
 
 private:
 	//Traject
-	//here comes some trajectory storage (to store the k best traj from the all prev iterations)
-};
-
-struct Traject{
-public:
-	Traject();
-	~Traject() = default;
-
-private:
 	//here comes some trajectory storage (to store the k best traj from the all prev iterations)
 };
